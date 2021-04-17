ca557163e8aa53f086cc17afec536080a135f189
bmumz <brandimummery@gmail.com>
2021-04-16 22:18:33 -0400

    Improved algo logic. Added errors for test cases.

--- src/helpers/getNthFibNum.js
+++ src/helpers/getNthFibNum.js
@@ -1,8 +1,27 @@
 const getNthFibNum = (n) => {
+  if (n <= 0) {
+    throw Error("n cannot be 0 or a negative integer.");
+  }
+  if (n > 500) {
+    throw Error("n cannot be greater than 500.");
+  }
   if (n <= 1) {
     return n;
   }
-  return getNthFibNum(n - 1) + getNthFibNum(n - 2);
+  if (n === 2) {
+    return 1;
+  }
+
+  let currentNum = 0;
+  let pastNum1 = 1;
+  let pastNum2 = 1;
+
+  for (let i = 3; i <= n; i++) {
+    currentNum = pastNum1 + pastNum2;
+    pastNum2 = pastNum1;
+    pastNum1 = currentNum;
+  }
+  return currentNum;
 };
 
 export default getNthFibNum;
--- src/helpers/getNthFibNum.test.js
+++ src/helpers/getNthFibNum.test.js
@@ -1,6 +1,23 @@
 import getNthFibNum from "./getNthFibNum";
 
 describe("getNthFibNum(n)", () => {
+  it("Should throw error if n is a negative integer.", () => {
+    expect(() => getNthFibNum(-2)).toThrowError(
+      "n cannot be 0 or a negative integer."
+    );
+  });
+
+  it("Should throw error if n is 0.", () => {
+    expect(() => getNthFibNum(0)).toThrowError(
+      "n cannot be 0 or a negative integer."
+    );
+  });
+
+  it("Should throw error if n is greater than 500.", () => {
+    expect(() => getNthFibNum(550)).toThrowError(
+      "n cannot be greater than 500."
+    );
+  });
   it("Should return 1 as first Fibonacci number.", () => {
     expect(getNthFibNum(1)).toEqual(1);
   });
